735b57111b6bd568265d729d5f92cde0156614d1
je-99 <ja.ehr99@gmail.com>
2020-12-11 16:11:32 +0100

    Update DbSensorPush.py

--- scripts/DbSensorPush.py
+++ scripts/DbSensorPush.py
@@ -1,10 +1,17 @@
 import requests
+from sense_hat import SenseHat
 
-temperature = 18.98
-humidity = 51.23
-pressure = 998.21
 
-def postDataToDb:
-    payload = 'temperature value=' + temperature
+def postDataToDb(temp, humid, pres):
+    payload = 'temperature value=' + temp + '\n' + 'humidity value=' + humid + '\n' + 'pressure value=' + pres
 
     r = requests.post("http://localhost:8086/write?db=sensor", data=payload)
+
+
+sense = SenseHat()
+
+temperature = sense.temp
+humidity = sense.humidity
+pressure = sense.pressure
+
+postDataToDb(str(round(temperature, 2)), str(round(humidity, 2)), str(round(pressure, 2)))
