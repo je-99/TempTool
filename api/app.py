20d70788af72da414a0fe1ba8c7eff911914dec2
je-99 <ja.ehr99@gmail.com>
2020-12-26 20:48:06 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -63,10 +63,10 @@ def routeAll():
     return apiAll()
 
 # Serve React App
- @app.route('/', defaults={'path': ''})
- @app.route('/<path:path>')
- def serve(path):
-     if path != "" and os.path.exists(app.static_folder + '/' + path):
-         return send_from_directory(app.static_folder, path)
-     else:
-         return send_from_directory(app.static_folder, 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
